1911c806b4c99b39c82de9c54d6ba4d8ea2dc18c
James Hillyerd <james@hillyerd.com>
2023-02-10 13:43:16 -0800

    test: Rewrite tests to be table driven, with useful output
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- json_test.go
+++ json_test.go
@@ -2,97 +2,195 @@ package json
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
-	"github.com/yuin/gopher-lua"
+	lua "github.com/yuin/gopher-lua"
 )
 
-func TestSimple(t *testing.T) {
-	const str = `
-	local json = require("json")
-	assert(type(json) == "table")
-	assert(type(json.decode) == "function")
-	assert(type(json.encode) == "function")
+// LuaInit holds globals available to callers of newLuaState().
+const LuaInit = `
+	json = require("json")
 
-	assert(json.encode(true) == "true")
-	assert(json.encode(1) == "1")
-	assert(json.encode(-10) == "-10")
-	assert(json.encode(nil) == "null")
-	assert(json.encode({}) == "[]")
-	assert(json.encode({1, 2, 3}) == "[1,2,3]")
+	-- Tests plain values and list-style tables.
+	function assert_eq(got, want)
+		if type(got) == "table" and type(want) == "table" then
+			assert(#got == #want, string.format("got %d elements, wanted %d", #got, #want))
 
-	local _, err = json.encode({1, 2, [10] = 3})
-	assert(string.find(err, "sparse array"))
+			for i, gotv in ipairs(got) do
+				local wantv = want[i]
+				assert_eq(gotv, wantv, "got[%d] = %q, wanted %q", gotv, wantv)
+			end
 
-	local _, err = json.encode({1, 2, 3, name = "Tim"})
-	assert(string.find(err, "mixed or invalid key types"))
+			return
+		end
 
-	local _, err = json.encode({name = "Tim", [false] = 123})
-	assert(string.find(err, "mixed or invalid key types"))
-
-	local obj = {"a",1,"b",2,"c",3}
-	local jsonStr = json.encode(obj)
-	local jsonObj = json.decode(jsonStr)
-	for i = 1, #obj do
-		assert(obj[i] == jsonObj[i])
+		assert(got == want, string.format("got %q, wanted %q", got, want))
 	end
 
-	local obj = {name="Tim",number=12345}
-	local jsonStr = json.encode(obj)
-	local jsonObj = json.decode(jsonStr)
-	assert(obj.name == jsonObj.name)
-	assert(obj.number == jsonObj.number)
-
-	assert(json.decode("null") == nil)
-
-	local status, err = pcall(function() json.decode() end)
-	assert(err == "<string>:38: bad argument #1 to decode", err)
-	local status, err = pcall(function() json.decode(1,2) end)
-	assert(err == "<string>:40: bad argument #1 to decode", err)
-	local status, err = pcall(function() json.encode() end)
-	assert(err == "<string>:42: bad argument #1 to encode", err)
-	local status, err = pcall(function() json.encode(1,2) end)
-	assert(err == "<string>:44: bad argument #1 to encode", err)
-
-	assert(json.decode(json.encode({person={name = "tim",}})).person.name == "tim")
-
-	local obj = {
-		abc = 123,
-		def = nil,
-	}
-	local obj2 = {
-		obj = obj,
-	}
-	obj.obj2 = obj2
-	assert(json.encode(obj) == nil)
-
-	local a = {}
-	for i=1, 5 do
-		a[i] = i
+	function assert_contains(got, want)
+		assert(string.find(got, want),
+			string.format("got %q, wanted it to contain %q", got, want))
 	end
-	assert(json.encode(a) == "[1,2,3,4,5]")
+`
+
+func TestRequire(t *testing.T) {
+	const code = `
+		local j = require("json")
+		assert(type(j) == "table")
+		assert(type(j.decode) == "function")
+		assert(type(j.encode) == "function")
 	`
-	s := lua.NewState()
-	defer s.Close()
 
-	s.PreloadModule("json", Loader)
-	if err := s.DoString(str); err != nil {
+	ls := lua.NewState()
+	defer ls.Close()
+
+	ls.PreloadModule("json", Loader)
+	if err := ls.DoString(code); err != nil {
 		t.Error(err)
 	}
 }
 
-func TestCustomRequire(t *testing.T) {
-	const str = `
-	local j = require("JSON")
-	assert(type(j) == "table")
-	assert(type(j.decode) == "function")
-	assert(type(j.encode) == "function")
-	`
-	s := lua.NewState()
-	defer s.Close()
+func TestEncodeValues(t *testing.T) {
+	tcs := map[string]struct {
+		expr, want string
+	}{
+		"true":         {"true", "true"},
+		"false":        {"false", "false"},
+		"integer":      {"42", "42"},
+		"negative int": {"-10", "-10"},
+		"float":        {"1.234", "1.234"},
+		"nil":          {"nil", "null"},
+		"empty list":   {"{}", "[]"},
+		"number list":  {"{1, 2, 3}", "[1,2,3]"},
+		"string list":  {"{'a', 'b', 'c'}", `["a","b","c"]`},
+	}
 
-	s.PreloadModule("JSON", Loader)
-	if err := s.DoString(str); err != nil {
+	ls := newLuaState()
+	defer ls.Close()
+
+	for name, tc := range tcs {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			code := fmt.Sprintf("assert_eq(json.encode(%s), %q)\n", tc.expr, tc.want)
+			if err := ls.DoString(code); err != nil {
+				t.Error(code, err)
+			}
+		})
+	}
+}
+
+func TestEncodeErrors(t *testing.T) {
+	const code = `
+		local _, err = json.encode({1, 2, [10] = 3})
+		assert_contains(err, "sparse array")
+
+		local _, err = json.encode({1, 2, 3, name = "Tim"})
+		assert_contains(err, "mixed or invalid key types")
+
+		local _, err = json.encode({name = "Tim", [false] = 123})
+		assert_contains(err, "mixed or invalid key types")
+	`
+
+	ls := newLuaState()
+	defer ls.Close()
+
+	if err := ls.DoString(code); err != nil {
+		t.Error(code, err)
+	}
+}
+
+func TestDecodeValues(t *testing.T) {
+	tcs := map[string]struct {
+		input, expr string
+	}{
+		"true":         {"true", "true"},
+		"false":        {"false", "false"},
+		"integer":      {"42", "42"},
+		"negative int": {"-10", "-10"},
+		"float":        {"1.234", "1.234"},
+		"null":         {"null", "nil"},
+		"empty list":   {"[]", "{}"},
+		"number list":  {"[1, 2, 3]", "{1, 2, 3}"},
+		"string list":  {`["a", "b", "c"]`, "{'a', 'b', 'c'}"},
+	}
+
+	ls := newLuaState()
+	defer ls.Close()
+
+	for name, tc := range tcs {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			code := fmt.Sprintf(`
+				local got, err = json.decode(%q)
+				if err ~= nil then error(err) end
+				assert_eq(got, %s)
+			`, tc.input, tc.expr)
+
+			if err := ls.DoString(code); err != nil {
+				t.Error(code, err)
+			}
+		})
+	}
+}
+
+func TestIncorrectArgLens(t *testing.T) {
+	const code = `
+		local status, err = pcall(function() json.decode() end)
+		assert_contains(err, "bad argument #1 to decode")
+
+		local status, err = pcall(function() json.decode(1,2) end)
+		assert_contains(err, "bad argument #1 to decode")
+
+		local status, err = pcall(function() json.encode() end)
+		assert_contains(err, "bad argument #1 to encode")
+
+		local status, err = pcall(function() json.encode(1,2) end)
+		assert_contains(err, "bad argument #1 to encode")
+	`
+
+	ls := newLuaState()
+	defer ls.Close()
+
+	if err := ls.DoString(code); err != nil {
+		t.Error(code, err)
+	}
+}
+
+func TestComplexCases(t *testing.T) {
+	const code = `
+		-- Table round-trip.
+		local obj = {"a",1,"b",2,"c",3}
+		local jsonStr = json.encode(obj)
+		local jsonObj = json.decode(jsonStr)
+		assert_eq(jsonObj, obj)
+
+		-- Table round-trip.
+		local obj = {name="Tim",number=12345}
+		local jsonStr = json.encode(obj)
+		local jsonObj = json.decode(jsonStr)
+		assert_eq(jsonObj.name, obj.name)
+		assert_eq(jsonObj.number, obj.number)
+
+		-- Table round-trip.
+		assert(json.decode(json.encode({person={name = "tim",}})).person.name == "tim")
+
+		-- Recursion.
+		local obj = {
+			abc = 123,
+			def = nil,
+		}
+		local obj2 = {
+			obj = obj,
+		}
+		obj.obj2 = obj2
+		assert(json.encode(obj) == nil)
+	`
+
+	ls := newLuaState()
+	defer ls.Close()
+
+	if err := ls.DoString(code); err != nil {
 		t.Error(err)
 	}
 }
@@ -105,4 +203,15 @@ func TestDecodeValue_jsonNumber(t *testing.T) {
 	if v.Type() != lua.LTString || v.String() != "124.11" {
 		t.Fatalf("expecting LString, got %T", v)
 	}
+}
+
+func newLuaState() *lua.LState {
+	// Initialize Lua.
+	ls := lua.NewState()
+	ls.PreloadModule("json", Loader)
+	if err := ls.DoString(LuaInit); err != nil {
+		panic(err)
+	}
+
+	return ls
 }
